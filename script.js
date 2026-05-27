9aab7e1b820eb11fada8e624ceea7b7f087db5f1
TheRedKardinal <claudio_giasi@icloud.com>
2026-05-27 12:36:55 +0200

    Creazione della notifica sul cerchio iniziata e quasi ultimata.

--- script.js
+++ script.js
@@ -298,7 +298,7 @@ let SELECTED_QUESTIONS = [];
    COSTANTI
 ========================= */
 
-const TIMER_DURATION = 5;
+const TIMER_DURATION = 1;
 const PASS_THRESHOLD = 60;
 
 /* =========================
@@ -510,173 +510,146 @@ function startTimer() {
   }, 1000);
 }
 
-    function stopTimer() {
-      clearInterval(timerId);
-    }
+function stopTimer() {
+  clearInterval(timerId);
+}
 
-    /* =========================
-       RESULTS PAGE
-    ========================= */
+/* =========================
+   RESULTS PAGE
+========================= */
 
-    function showResult() {
-      app.innerHTML = "";
+function showResult() {
+  app.innerHTML = "";
 
-<<<<<<< HEAD
-  const percentage = Math.round((correctAnswers / SELECTED_QUESTIONS.length) * 100);
-=======
-      const percentage = Math.round(
-        (correctAnswers / SELECTED_QUESTIONS.length) * 100,
-      );
->>>>>>> main
+  const percentage = Math.round(
+    (correctAnswers / SELECTED_QUESTIONS.length) * 100,
+  );
 
-      const results = document.createElement("div");
-      results.classList.add("results");
+  const results = document.createElement("div");
+  results.classList.add("results");
 
-      const resultTitle = document.createElement("h1");
-      resultTitle.classList.add("result-title");
-      resultTitle.textContent = "Risultato";
+  const resultTitle = document.createElement("h1");
+  resultTitle.classList.add("result-title");
+  resultTitle.textContent = "Risultato";
 
-      const verdict = document.createElement("h2");
+  const verdict = document.createElement("h2");
 
-      if (percentage >= PASS_THRESHOLD) {
-        verdict.textContent = "PROMOSSO";
-        verdict.classList.add("passed");
-      } else {
-        verdict.textContent = "BOCCIATO";
-        verdict.classList.add("failed");
-      }
+  if (percentage >= PASS_THRESHOLD) {
+    verdict.textContent = "PROMOSSO";
+    verdict.classList.add("passed");
+  } else {
+    verdict.textContent = "BOCCIATO";
+    verdict.classList.add("failed");
+  }
 
-      const resultList = document.createElement("ul");
-      resultList.classList.add("result-list");
+  const resultList = document.createElement("ul");
+  resultList.classList.add("result-list");
 
-      const correctText = document.createElement("li");
-      correctText.textContent = `Risposte corrette: ${correctAnswers}`;
+  const correctText = document.createElement("li");
+  correctText.textContent = `Risposte corrette: ${correctAnswers}`;
 
-      const wrongText = document.createElement("li");
-      wrongText.textContent = `Risposte sbagliate: ${wrongAnswers}`;
+  const wrongText = document.createElement("li");
+  wrongText.textContent = `Risposte sbagliate: ${wrongAnswers}`;
 
-      const scoreText = document.createElement("li");
-      scoreText.textContent = `Punteggio finale: ${percentage}%`;
+  const scoreText = document.createElement("li");
+  scoreText.textContent = `Punteggio finale: ${percentage}%`;
 
-      resultList.appendChild(correctText);
-      resultList.appendChild(wrongText);
-      resultList.appendChild(scoreText);
+  resultList.appendChild(correctText);
+  resultList.appendChild(wrongText);
+  resultList.appendChild(scoreText);
 
-      // Creazione di percentuale circolare - prova con svg (disegno tecnico vettoriale)
-      const svgDictionary = "http://www.w3.org/2000/svg"; // dichiarazione del dizionario vettoriale
+  // Creazione di percentuale circolare - prova con svg (disegno tecnico vettoriale)
+  const svgDictionary = "http://www.w3.org/2000/svg"; // dichiarazione del dizionario vettoriale
 
-      // contenitore del cerchio
-      const percentageContainer = document.createElementNS(svgDictionary, "svg"); // dico a JS di creare tramite svg e non html, (dizionario, oggetto da costruire)
-      percentageContainer.classList.add("percentage-container");
+  // creazione del div contenitore di tutto per attaccare la notifica
+  const divDelCerchio = document.createElement('div');
+  divDelCerchio.classList.add('circle-div');
+  // div per la notifica 
+  const notifica = document.createElement('div');
+  notifica.textContent = `Hai risposto correttamente a ${correctAnswers} su ${QUESTIONS.length}`;
+  notifica.classList.add('notifica');
 
-      // cerchio
-      const percentageCircle = document.createElementNS(svgDictionary, "circle");
-      percentageCircle.classList.add("percentage-circle");
 
-<<<<<<< HEAD
+  // contenitore del cerchio
+  const percentageContainer = document.createElementNS(svgDictionary, "svg"); // dico a JS di creare tramite svg e non html, (dizionario, oggetto da costruire)
+  percentageContainer.classList.add("percentage-container");
+
+  // cerchio
+  const percentageCircle = document.createElementNS(svgDictionary, "circle");
+  percentageCircle.classList.add("percentage-circle");
+
   // cerchio vuoto
-  const EmptyBarCircle = document.createElementNS(svgDictionary, 'circle');
-  EmptyBarCircle.classList.add('empty-bar-circle');
+  const EmptyBarCircle = document.createElementNS(svgDictionary, "circle");
+  EmptyBarCircle.classList.add("empty-bar-circle");
 
-  EmptyBarCircle.setAttribute('cx', '100') // asse orizzontale
-  EmptyBarCircle.setAttribute('cy', '100') // asse verticale
-  EmptyBarCircle.setAttribute('r', '90') // perimetro
+  EmptyBarCircle.setAttribute("cx", "100"); // asse orizzontale
+  EmptyBarCircle.setAttribute("cy", "100"); // asse verticale
+  EmptyBarCircle.setAttribute("r", "90"); // perimetro
 
   // bisogna dichiarare le misure geometriche
-  percentageCircle.setAttribute('cx', '100') // asse orizzontale
-  percentageCircle.setAttribute('cy', '100') // asse verticale
-  percentageCircle.setAttribute('r', '90') // perimetro
+  percentageCircle.setAttribute("cx", "100"); // asse orizzontale
+  percentageCircle.setAttribute("cy", "100"); // asse verticale
+  percentageCircle.setAttribute("r", "90"); // perimetro
   // remind: area del cerchio: C = 2 * pi * r. - 2 * 3.141259 * 90 = 565.4
-=======
-      // cerchio vuoto
-      const EmptyBarCircle = document.createElementNS(svgDictionary, "circle");
-      EmptyBarCircle.classList.add("empty-bar-circle");
->>>>>>> main
 
-      EmptyBarCircle.setAttribute("cx", "100"); // asse orizzontale
-      EmptyBarCircle.setAttribute("cy", "100"); // asse verticale
-      EmptyBarCircle.setAttribute("r", "90"); // perimetro
+  // scritta nel cerchio
+  const percentageText = document.createElementNS(svgDictionary, "text");
+  percentageText.classList.add("percentage-text");
+  percentageText.textContent = `${percentage}%`;
 
-      // bisogna dichiarare le misure geometriche
-      percentageCircle.setAttribute("cx", "100"); // asse orizzontale
-      percentageCircle.setAttribute("cy", "100"); // asse verticale
-      percentageCircle.setAttribute("r", "90"); // perimetro
-      // remind: area del cerchio: C = 2 * pi * r. - 2 * 3.141259 * 90 = 565.4
+  // coordinate geometriche del text
+  percentageText.setAttribute("x", "80");
+  percentageText.setAttribute("y", "115");
 
-<<<<<<< HEAD
-  percentageContainer.appendChild(percentageText);
   percentageContainer.appendChild(EmptyBarCircle);
   percentageContainer.appendChild(percentageCircle);
-=======
-      // scritta nel cerchio
-      const percentageText = document.createElementNS(svgDictionary, "text");
-      percentageText.classList.add("percentage-text");
-      percentageText.textContent = `${percentage}%`;
->>>>>>> main
+  percentageContainer.appendChild(percentageText);
 
-      // coordinate geometriche del text
-      percentageText.setAttribute("x", "80");
-      percentageText.setAttribute("y", "115");
+  // riempimento della barra percentuale
+  if (percentage >= 80) {
+    // definisco ciclo if/ else if per cambiare il colore a seconda della percnetuale
+    percentageCircle.style.stroke = "lightgreen";
+    percentageText.style.stroke = "lightgreen";
+  } else if (percentage >= 60 && percentage <= 79) {
+    percentageCircle.style.stroke = "yellow";
+    percentageText.style.stroke = "yellow";
+  } else {
+    percentageCircle.style.stroke = "red";
+    percentageText.style.stroke = "red";
+  }
 
-<<<<<<< HEAD
   const circonferenza = 565.4;
   percentageCircle.style.strokeDasharray = 564.4; // Lunghezza piena del riempimento
   // valore che fa tornare indietro la colorazione (deve essere la circonferenza - (la circonferenza * (la percentuale/ 100))
   const offsetValue = circonferenza - (percentage / 100) * circonferenza;
   percentageCircle.style.strokeDashoffset = offsetValue;
-=======
-      percentageContainer.appendChild(percentageText);
-      percentageContainer.appendChild(EmptyBarCircle);
-      percentageContainer.appendChild(percentageCircle);
->>>>>>> main
 
-      // riempimento della barra percentuale
-      if (percentage >= 80) {
-        // definisco ciclo if/ else if per cambiare il colore a seconda della percnetuale
-        percentageCircle.style.stroke = "lightgreen";
-        percentageText.style.stroke = "lightgreen";
-      } else if (percentage >= 60 && percentage <= 79) {
-        percentageCircle.style.stroke = "yellow";
-        percentageText.style.stroke = "yellow";
-      } else {
-        percentageCircle.style.stroke = "red";
-        percentageText.style.stroke = "red";
-      }
+  // funzione della notifica
+  percentageText.addEventListener('mouseover', (e) => {
+    notifica.style.opacity = '1';
+    notifica.style.visibility = 'visible';
+  });
+  percentageText.addEventListener('mouseleave', (e) => {
+    notifica.style.visibility = 'hidden';
+  });
 
-      const circonferenza = 565.4;
-      percentageCircle.style.strokeDasharray = 564.4; // Lunghezza piena del riempimento
-      // valore che fa tornare indietro la colorazione (deve essere la circonferenza - (la circonferenza * (la percentuale/ 100))
-      const offsetValue = circonferenza - (percentage / 100) * circonferenza;
-      percentageCircle.style.strokeDashoffset = offsetValue;
+  const restartButton = document.createElement("button");
+  restartButton.classList.add("result-button");
+  restartButton.textContent = "RIPROVA";
 
-      const restartButton = document.createElement("button");
-      restartButton.classList.add("result-button");
-      restartButton.textContent = "RIPROVA";
-
-      restartButton.addEventListener("click", () => {
-        showWelcome();
-      });
-
-      results.appendChild(resultTitle);
-      results.appendChild(verdict);
-      results.appendChild(resultList);
-      results.appendChild(percentageContainer);
-      results.appendChild(restartButton);
-
-      app.appendChild(results);
-    }
-
-    /* =========================
-       START
-    ========================= */
-
+  restartButton.addEventListener("click", () => {
     showWelcome();
+  });
 
-<<<<<<< HEAD
+  // append child per hover del cerchio
+  divDelCerchio.appendChild(percentageContainer);
+  divDelCerchio.appendChild(notifica);
+
+
   results.appendChild(resultTitle);
   results.appendChild(verdict);
   results.appendChild(resultList);
-  results.appendChild(percentageContainer);
+  results.appendChild(divDelCerchio);
   results.appendChild(restartButton);
 
   app.appendChild(results);
@@ -686,7 +659,6 @@ function startTimer() {
    START
 ========================= */
 
-showWelcome();
-=======
+showResult();
+
 // 8======D
->>>>>>> main
